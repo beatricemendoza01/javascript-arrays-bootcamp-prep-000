85443ef0153e0914ffe4725173a41305c64cb797
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-15 22:51:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,6 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, elemen) {
-  return array.unsh
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
 }
