fb7bc26f5b685aaf6b4ace0a5f8781616ab38666
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-15 22:58:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  
+  var new_array = array.slice(1);
+  return new_array;
 }
